0f6c857b47c23ccabf573c46d4937f890aa96bf6
ssreddy2020 <ssreddy2020@gmail.com>
2016-02-23 22:55:44 -0500

    Added comments and few changes
    Added input key text file, comments and few changes to driver.java and
    aescipher.java

--- AES/driver.java
+++ AES/driver.java
@@ -1,23 +1,35 @@
+/**
+ * file: driver.java
+ * author: Sandeep Reddy Salla
+ * course: MSCS_630L_231_16S 
+ * assignment: Lab 2
+ * due date: February 23, 2016
+ * version: 1.0
+ * 
+ * This file contains the declaration of driver class
+ */
+
 import java.util.Scanner;
 
+/**
+ * This class is used to take the input from the scanner and calls the advanced
+ * encryption round key method by providing the input
+ */
 public class driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
-		Scanner sc =  new Scanner(System.in);
+		// TODO Auto-generated method stub
+		// Scanner is used to read input from the console
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the key:");
 		String input = sc.nextLine();
-		if(input.length()==32){			
+		// condition to check whether the input length is 32 or not
+		if (input.length() == 32) {
 			aescipher.aesRoundKeys(input);
+			// closes the scanner
 			sc.close();
-		}
-		else
-		{
+		} else {
 			System.out.println("Input length is not 32");
-		}			 
+		}
 	}
 }
-
-
-
-
